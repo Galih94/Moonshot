1a4409fd7a6702e75a342aba2dc386d816b410e0
galih <samudragalih94@gmail.com>
2024-07-11 13:57:59 +0700

    wip initial project

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Galih Samudra on 11/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
