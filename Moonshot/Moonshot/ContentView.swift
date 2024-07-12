5f65c7532465c61034716bc669fe43946ea3668b
galih <samudragalih94@gmail.com>
2024-07-12 11:29:04 +0700

    feat(MIssions): read missions.json as object Mission array

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -9,11 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     let astronaus: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
     
     var body: some View {
         ScrollView {
             VStack {
                 Text("print \(astronaus)")
+            }
+            Spacer()
+            VStack {
+                Text("print \(missions)")
             }
         }
     }
