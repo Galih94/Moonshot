e5b91fd4e3fcb1a0e1627ea307af37fe43c116d4
galih <samudragalih94@gmail.com>
2024-07-12 10:38:50 +0700

    feat(Bundle): create extension Bundle to decode JSON, show in UI result of astronaut

--- Moonshot/Moonshot/Astronaut.swift
+++ Moonshot/Moonshot/Astronaut.swift
@@ -12,3 +12,21 @@ struct Astronaut: Codable, Identifiable {
     let name: String
     let description: String
 }
+
+extension Bundle {
+    func decode(_ file: String) -> [String: Astronaut] {
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle")
+        }
+        
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to get data from \(url)")
+        }
+        
+        guard let loadedData = try? JSONDecoder().decode([String: Astronaut].self, from: data) else {
+            fatalError("Failed to decode data from \(url)")
+        }
+        
+        return loadedData
+    }
+}
--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -8,9 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    let astronaus = Bundle.main.decode("astronauts.json")
     
     var body: some View {
-        Text("Hello world")
+        ScrollView {   
+            VStack {
+                Text("print \(astronaus)")
+            }
+        }
     }
 }
 
