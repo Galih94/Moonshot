68ec97bf2cc313c4cbc37ce69035dd0a46606dbf
galih <samudragalih94@gmail.com>
2024-07-12 10:34:18 +0700

    remove(code): clean code to make it default View

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -7,41 +7,10 @@
 
 import SwiftUI
 
-
-struct CustomText: View {
-    let text: String
-    var body: some View {
-        Text(text)
-    }
-    
-    init(_ text: String) {
-        self.text = text
-    }
-}
-
-struct Address: Codable {
-    let street: String
-    let city: String
-}
-
-struct User: Codable {
-    let name: String
-    let address: Address
-}
-
 struct ContentView: View {
-    let layout = [
-        GridItem(.adaptive(minimum: 50, maximum: 100)),
-    ]
     
     var body: some View {
-        ScrollView(.horizontal) {
-            LazyHGrid(rows: layout) {
-                ForEach(0..<300) { index in
-                    Text("Item \(index)")
-                }
-            }
-        }
+        Text("Hello world")
     }
 }
 
