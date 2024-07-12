3477a7a5a54397a56d0c7ab45a047d7ca95060f1
galih <samudragalih94@gmail.com>
2024-07-12 17:14:15 +0700

    feat(UI): udpate UI cell of LazyVGrid View

---
+++ Moonshot/Moonshot/Color-Theme.swift
@@ -0,0 +1,18 @@
+//
+//  Color-Theme.swift
+//  Moonshot
+//
+//  Created by Galih Samudra on 12/07/24.
+//
+
+import SwiftUI
+
+extension ShapeStyle where Self == Color {
+    static var darkBackground: Color {
+        return Color(red: 0.1, green: 0.1, blue: 0.2)
+    }
+    
+    static var lightBackground: Color {
+        return Color(red: 0.2, green: 0.2, blue: 0.3)
+    }
+}
--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -28,17 +28,26 @@ struct ContentView: View {
                                     .resizable()
                                     .scaledToFit()
                                     .frame(width: 100, height: 100)
+                                    .padding()
                                 VStack {
                                     Text(mission.displayName)
                                         .font(.headline)
                                     Text(mission.formattedLaunchDate)
                                         .font(.caption)
                                 }
+                                .padding(.vertical)
                                 .frame(maxWidth: .infinity)
+                                .background(.lightBackground)
+                            }
+                            .clipShape(.rect(cornerRadius: 10))
+                            .overlay {
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.lightBackground)
                             }
                         }
                     }
                 }
+                .padding([.horizontal, .bottom])
             }
             .navigationTitle("Moonshot")
         }
