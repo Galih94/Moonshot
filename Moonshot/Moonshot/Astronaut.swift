ee5690fb645cf42488f987ee10d5a149eada59f7
galih <samudragalih94@gmail.com>
2024-07-12 10:20:29 +0700

    feat(Model): create model Astronaut

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Galih Samudra on 12/07/24.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
