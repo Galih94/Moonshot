10951f2bdb61cfaf978fd1268ff51e76079f08f8
galih <samudragalih94@gmail.com>
2024-07-15 16:18:09 +0700

    refactor(UI): refactor to show list of mission using Grid or List UI

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -7,7 +7,89 @@
 
 import SwiftUI
 
+struct CustomGridMissionView: View {
+    let layout: [GridItem]
+    let missions: [Mission]
+    let astronaus: [String: Astronaut]
+    var body: some View {
+        LazyVGrid(columns: layout) {
+            ForEach(missions) { mission in
+                NavigationLink {
+                    MissionView(mission: mission, astronauts: astronaus)
+                } label: {
+                    VStack {
+                        Image(mission.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100, height: 100)
+                            .padding()
+                        VStack {
+                            Text(mission.displayName)
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                            Text(mission.formattedLaunchDate)
+                                .font(.caption)
+                                .foregroundStyle(.white.opacity(0.5))
+                        }
+                        .padding(.vertical)
+                        .frame(maxWidth: .infinity)
+                        .background(.lightBackground)
+                    }
+                    .clipShape(.rect(cornerRadius: 10))
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(.lightBackground)
+                    }
+                }
+            }
+        }
+        .padding([.horizontal, .bottom])
+    }
+}
+
+struct CustomListMissionView: View {
+    
+    let layout: [GridItem]
+    let missions: [Mission]
+    let astronaus: [String: Astronaut]
+    var body: some View {
+            ForEach(missions) { mission in
+                NavigationLink {
+                    MissionView(mission: mission, astronauts: astronaus)
+                } label: {
+                    HStack() {
+                        Image(mission.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 50, height: 50)
+                            .padding()
+                        VStack(alignment: .leading) {
+                            Text(mission.displayName)
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                            Text(mission.formattedLaunchDate)
+                                .font(.caption)
+                                .foregroundStyle(.white.opacity(0.5))
+                        }
+                        .padding(.vertical)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    }
+                    .background(.lightBackground)
+                    .clipShape(.rect(cornerRadius: 10))
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(.white)
+                    }
+                    .padding(.horizontal)
+                }
+                .frame(maxWidth: .infinity)
+        }
+        
+    }
+}
+
 struct ContentView: View {
+    @State private var isShowingGrid: Bool = false
     let astronaus: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
     
@@ -18,42 +100,22 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             ScrollView {
-                LazyVGrid(columns: layout) {
-                    ForEach(missions) { mission in
-                        NavigationLink {
-                            MissionView(mission: mission, astronauts: astronaus)
-                        } label: {
-                            VStack {
-                                Image(mission.image)
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 100, height: 100)
-                                    .padding()
-                                VStack {
-                                    Text(mission.displayName)
-                                        .font(.headline)
-                                        .foregroundStyle(.white)
-                                    Text(mission.formattedLaunchDate)
-                                        .font(.caption)
-                                        .foregroundStyle(.white.opacity(0.5))
-                                }
-                                .padding(.vertical)
-                                .frame(maxWidth: .infinity)
-                                .background(.lightBackground)
-                            }
-                            .clipShape(.rect(cornerRadius: 10))
-                            .overlay {
-                                RoundedRectangle(cornerRadius: 10)
-                                    .stroke(.lightBackground)
-                            }
-                        }
-                    }
+                if isShowingGrid {
+                    CustomGridMissionView(layout: layout, missions: missions, astronaus: astronaus)
+                } else {
+                    CustomListMissionView(layout: layout, missions: missions, astronaus: astronaus)
                 }
-                .padding([.horizontal, .bottom])
             }
             .navigationTitle("Moonshot")
             .background(.darkBackground)
             .preferredColorScheme(.dark)
+            .toolbar(content: {
+                Button {
+                    isShowingGrid.toggle()
+                } label: {
+                    Image(systemName: isShowingGrid ? "list.dash" : "square.grid.3x3.fill")
+                }
+            })
         }
     }
 }
